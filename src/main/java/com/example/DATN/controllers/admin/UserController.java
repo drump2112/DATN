04131da420adda660d89068c58825ea794eb2b6e
drump2112 <trumkhu1@gmail.com>
2025-07-15 22:24:52 +0700

    15/7 22:23 fix pagination, logout with layout

--- src/main/java/com/example/DATN/controllers/admin/UserController.java
+++ src/main/java/com/example/DATN/controllers/admin/UserController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/admin/employee")
